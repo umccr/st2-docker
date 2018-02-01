c11e2fb6a5f38403da0da60141f714500fee47cb
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-02-01 11:03:59 +1100

    Add startup scripts
    Add startup scripts to runtime folder in order to remove them from
    S3 config bucket, as they don't contain sensitiv information.
    [skip ci]

---
+++ runtime/st2.d/modify-cli-conf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+crudini --set /root/.st2/config cli timezone "Australia/Melbourne"
---
+++ runtime/st2.d/z_activate_components.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##### define active sensors
+st2 sensor enable arteria.IncomingSensor
+
+##### define active rules
+st2 rule enable umccr.umccr-runfolder-notify
+st2 rule enable umccr.on_bcl2fastq_success
+st2 rule enable umccr.on_bcl2fastq_failure
+st2 rule enable umccr.action_notify_slack_forwarder
+st2 rule enable pcgr.on_pcgr_s3_upload
+st2 rule disable examples.notify_slack
+st2 rule disable examples.sample_rule_file_watch
+st2 rule disable examples.sample_rule_with_actiontrigger
+st2 rule disable examples.sample_rule_with_webhook
+
+##### define active actions/workflows
+st2 action enable umccr.sync2hpc
+st2 action enable umccr.bcl2fastq_start

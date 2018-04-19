2189da367e87d4c55f1b954bf2d48c02a8f2f13d
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-04-19 12:02:13 +1000

    Add script to trigger stackstorm-ami build on travis success

---
+++ travis-remote-trigger.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+body='{
+"request": {
+  "message": "Build triggered by api request",
+  "branch":"master"
+}}'
+
+curl -v -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $TRAVIS_ACCESS_TOKEN" \
+   -d "$body" \
+   https://api.travis-ci.org/repo/umccr%2Fstackstorm-ami/requests
